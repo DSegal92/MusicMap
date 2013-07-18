54bf2d18837d2e9ad51ccfc1b4959d09f7e37b67
Daniel Segal <DSegal92@gmail.com>
2013-07-17 21:34:26 -0400

    Added new home screen

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ MusicMap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'artists#map'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
